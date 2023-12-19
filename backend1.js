cc3456d9117faf017f4bf7a27026127d2c877a3c
Harsh <106898396+HarshGurawaliya@users.noreply.github.com>
2023-12-19 18:46:48 +0100

    Update backend1.js

--- backend1.js
+++ backend1.js
@@ -6,30 +6,32 @@ const path = require('path');
 const { chatLogic } = require('./app'); 
 
 const app = express();
-app.use(cors()); 
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:3000", // Update for production
+  methods: ["GET", "POST"]
+}));
 
+// Serve static files from the frontend app
 app.use(express.static(path.join(__dirname, 'public')));
 
 const server = http.createServer(app);
 const io = socketIO(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: process.env.FRONTEND_URL || "http://localhost:3000", // Update for production
+    methods: ["GET", "POST"]
+  }
 });
 
 let userSessions = {}; 
 
 io.on('connection', (socket) => {
     console.log('A user connected: ' + socket.id);
-
     userSessions[socket.id] = {
         currentState: 'wStart', 
         orderInfo: {} 
     };
 
     socket.emit('botmes', "Welcome to Laurin Pasta and Pizza restaurant. Type 'yes' to start ordering your pizza.");
-
     socket.on('human', (data) => {
         chatLogic(socket, userSessions, data); 
     });
@@ -40,12 +42,12 @@ io.on('connection', (socket) => {
     });
 });
 
-
+// Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
