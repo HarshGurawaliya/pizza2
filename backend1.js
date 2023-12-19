d69f0c347a3ac6cdc7d41bf9bd4bf11efc231438
Harsh <106898396+HarshGurawaliya@users.noreply.github.com>
2023-12-19 19:04:31 +0100

    Update backend1.js

--- backend1.js
+++ backend1.js
@@ -47,7 +47,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
